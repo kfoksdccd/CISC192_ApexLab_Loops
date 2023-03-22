af06a75de9c8420ea02f2ba9c03bfd120f1e7767
kfoksdccd <126300842+kfoksdccd@users.noreply.github.com>
2023-03-21 18:14:56 -0700

    Add files via upload

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -5,52 +5,15 @@
 
 using namespace std;
 
-void middle_word_check(stringstream& cin, stringstream& cout)
-{
-    // This block of code takes a word from the user and sees if 
-    // it comes before or after the word "middle" in the dictionary 
-    // (lexicographical ordering) according to the ASCII table.
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
-
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+void email_concat(stringstream& cin, stringstream& cout) {
 }
 
-void animal_abcs(stringstream &cin, stringstream &cout) {
-    // It takes from input one character.  It outputs the name of 
-    // an animal that starts with the character.  We will only 
-    // process a-f and A-F.  Any other character will produce an error.  
-    // The lower case and capitol letters will produce the same animal 
-    // name in lower case.   Try not to duplicate code.
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
-
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+void donation_total(stringstream &cin, stringstream &cout) {
 }
 
-void delivery_range_check(std::stringstream &cin, std::stringstream &cout)
-{
-     //An UberEats driver needs to see if they can make 2 deliveries in a 
-     // 2 hour window, assuming they drive 45 mph on average.  If they 
-     // have to stop for gas or charge,  its a no go.  If the distance 
-     // between all stops is less than 15 miles and they are driving a 
-     // gas car, its more economical for the driver to take an electric 
-     // scooter.  
-     // This block takes 6 inputs:
-     //1. the car type, gas or electric
-     //2. the number of miles the car can go on a tank/charge
-     //3. 4 distances:
-     // - the driver's house to restaurant 1
-     // - restaurant 1 to customer 1
-     // - customer 1 to restaurant 2
-     // - restaurant 2 to customer 2
-     //This block outputs whether or not the deliver can be made and if it can what transportation the driver should use.
+void average_books_checked_out(std::stringstream &cin, std::stringstream &cout) {
+    
+}
 
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
-
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+void rectangles(std::stringstream &cin, std::stringstream &cout) {
 }
